e692c0485db371167de008276c945f9542cde662
Agnes Mwihaki <moringa@DESKTOP-89EPAHO.localdomain>
2026-01-08 09:58:01 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+function calculateTax(amount){
+    return amount * 0.10;
+}
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
+function findMaximum(num1, num2){
+    if(num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+
+function isPalindrome(word){
+    const original = word.toLowerCase();
+    const reversed = original.split("").reverse().join("");
+
+    return original === reversed;
+}
 
 
 
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    const discount = originalPrice * discountPercentage;  
+    return originalPrice - discount;
+}
 
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+
+
+findMaximum(167, 980);
+console.log(isPalindrome("Level"));
